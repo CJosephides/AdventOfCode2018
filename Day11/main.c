cd72f5b3e07041349e900a54b61af2e4110929e0
Christos Josephides <cjosephides@gmail.com>
2018-12-15 12:34:58 +0000

    Day11, part 2.

--- Day11/main.c
+++ Day11/main.c
@@ -8,6 +8,18 @@ int power(int y, int x, int s)
     return ((((x + 10) * y + s) * (x + 10) / 100) % 10) - 5;
 }
 
+int power_square(int y, int x, int k, int grid[SIDE][SIDE], int s)
+{
+    int power_sum = 0;
+    int i, j;
+    for (i = y; i < y + k; i++) {
+        for (j = x; j < x + k; j++) {
+            power_sum += grid[i][j];
+        }
+    }
+    return power_sum;
+}
+
 int main(int argc, char **argv)
 {
     /* Requires a single argument: the grid serial number. */
@@ -22,32 +34,27 @@ int main(int argc, char **argv)
         }
     }
 
-    /* Identify 3x3 square with highest power sum. */
+    /* Identify KxK square with highest power sum, for k in [1, 300]. */
+    int k;
     int power_sum;
     int max_power = -1000;
     int max_i, max_j;
-
-    for (i = 0; i < SIDE - 2; i++) {
-        for (j = 0; j < SIDE - 2; j++) {
-            power_sum =  0;
-            power_sum += power(i, j, serial);
-            power_sum += power(i, j+1, serial);
-            power_sum += power(i, j+2, serial);
-            power_sum += power(i+1, j, serial);
-            power_sum += power(i+1, j+1, serial);
-            power_sum += power(i+1, j+2, serial);
-            power_sum += power(i+2, j, serial);
-            power_sum += power(i+2, j+1, serial);
-            power_sum += power(i+2, j+2, serial);
-            if (power_sum > max_power) {
-                max_power = power_sum;
-                max_i = i;
-                max_j = j;
+    int max_k;
+    for (k = 1; k <= 300; k++) {
+        for (i = 0; i <= SIDE - k; i++) {
+            for (j = 0; j <= SIDE - k; j++) {
+                power_sum = power_square(i, j, k, grid, serial);
+                if (power_sum > max_power) {
+                    max_i = i;
+                    max_j = j;
+                    max_k = k;
+                    max_power = power_sum;
+                }
             }
         }
+        printf("Finished k = %d.\n", k);
+        printf("X = %d, Y = %d, K = %d. Power = %d.\n", max_j, max_i, max_k, max_power);
     }
-
-    printf("Maximum 3x3 power square @ X=%d, Y=%d (power = %d).\n", max_j, max_i, max_power);
 
     return 0;
 }
